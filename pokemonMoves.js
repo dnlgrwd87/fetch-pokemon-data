dab09c112f90f46541575e4385c06a04f0173a42
Daniel <dgarwood@uncc.edu>
2018-07-02 14:38:31 -0400

    really fixed egg moves this time lol

--- pokemonMoves.js
+++ pokemonMoves.js
@@ -5,7 +5,7 @@ let fetechedMoves = [];
 let fetechedMovesIndex = [];
 let savedCalls = 0;
 
-for (let i = 10; i <= 20; i++) {
+for (let i = 1; i <= 9; i++) {
   getMove(i);
   // db.collection("pokemonMoves")
   // .doc(i.toString())
